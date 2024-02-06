b52fbab55a36e99de0ebacec9cf7ec5821e8bf80
haejin.yang91 <haejin.yang91@gmail.com>
2024-02-07 04:54:07 +0900

    docs: php-apache에 env적용할 수 있게 세팅.
    env를 적용한 최초의 방식이 맞았는데, php의 ini파일에서 zend_extension이 중복으로 실행되었고, 그것이 아파치까지 영향을 끼쳤던것

--- php-apache/start.sh
+++ php-apache/start.sh
@@ -1,12 +1,27 @@
-if [ "$APP_ENV" = "production" ]; then
+#!/bin/bash
+env_file="./.env"
+if [ -r "$env_file" ]; then
+    source "$env_file"
+    echo "$env_file file was sourced successfully."
+else
+    echo "$env_file file does not exist."
+fi
+
+# php ini 파일을 실행 모드에 맞게 관리한다.
+if [ "$APP_MODE" = "production" ]; then
+    echo "run production"
     rm -f /usr/local/etc/php/conf.d/development.ini
     rm -f /usr/local/etc/php/conf.d/release.ini
-elif [ "$APP_ENV" = "release" ]; then
+elif [ "$APP_MODE" = "release" ]; then
+    echo "run release"
     rm -f /usr/local/etc/php/conf.d/development.ini
+    rm -f /usr/local/etc/php/conf.d/production.ini
+elif [ "$APP_MODE" = "development" ]; then
+    echo "run development"
+    rm -f /usr/local/etc/php/conf.d/release.ini
     rm -f /usr/local/etc/php/conf.d/production.ini
 else
-    rm -f /usr/local/etc/php/conf.d/release.ini
-    rm -f /usr/local/etc/php/conf.d/production.ini
+    echo "invalid case : $APP_MODE"
 fi
 
 # cron
